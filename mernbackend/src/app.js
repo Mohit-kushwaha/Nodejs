5a09a3a96ff8f219b2bfac4d2e98f24361603930
Mohit-kushwaha <mohit_203015@saitm.org>
2022-02-28 11:27:14 +0530

    new change

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,27 +1,28 @@
 const express = require("express");
 const path = require('path')
- require("./db/conn");
+require("./db/conn");
 const app = express();
 const port = process.env.PORT || 3000
 const hbs = require('hbs')
 
 
 
-const static_path = path.join(__dirname,"../public")
-const template_path = path.join(__dirname,"../templates/views")
+const static_path = path.join(__dirname, "../public")
+const template_path = path.join(__dirname, "../views")
 console.log(template_path)
-const partials_path = path.join(__dirname,"../templates/partials")
-
-app.use(express.static( static_path ))
+const partials_path = path.join(__dirname, "../partials")
+console.log(partials_path)
+app.use(express.static(static_path))
 app.set("view engine", "hbs")
-app.set("views",template_path)
 
 
+app.set("views", template_path)
 hbs.registerPartials(partials_path)
 
 
-app.get("/", (req,res)=>{
-res.render("index")
+app.get("/", (req, res) =>
+{
+    res.render("index")
 })
 
-app.listen(port , ()=>{console.log(`connection Successful at ${port}`)})
+app.listen(port, () => { console.log(`connection Successful at ${ port }`) })
